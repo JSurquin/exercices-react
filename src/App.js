953047aa7ef63aaf9ed3d2d22d457ee5f9403dd3
Jimmylan <jimmylan.surquin@andromed.fr>
2022-06-09 11:19:00 +0200

    refactor: remove unused components in App.jsx, create App2.jsx in functional components and inject in the /src/index.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import Clock from './components/Clock';
-import Toggle from './components/Toggle';
-import MonComposant from './components/MonComposantAEtat';
+// import Clock from './components/Clock';
+// import Toggle from './components/Toggle';
+// import MonComposant from './components/MonComposantAEtat';
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Button from './components/Button';
 
+
 class App extends React.Component {
-  
   constructor(props)
   {
     super(props);
     this.state = {count: 0}
-    //this.updateMyCount = this.updateMyCount.bind(this);
-  }
+  };
 
   updateMyCount = () => {
-    // this.setState({count: this.state.count + 1});
     this.setState(state => ({count: state.count + 1}))
   }
 
@@ -23,24 +21,15 @@ class App extends React.Component {
   {
     return (
       <div className="App" style={{border: '2px solid green', padding: 20}}>
-        {/* <MonComposant/>
-        <Toggle/>
-        <Clock/> */}
-        
-        {this.state.count}
-        {/* est équivalent à App.state.count  */}
-
-
-        <Button onClick={() => this.updateMyCount()}>
-          mon texte dynamique
-          </Button>
-
-{/* pour faire qu'on peut changer le texte , via la props children qui se créer automatiquement */}
-          <Button>
-          le texte change alors que c'est le meme composant
-          </Button>
-      </div>
-    );
+      {this.state.count}
+      <Button onClick={() => this.updateMyCount()}>
+        mon texte dynamique
+        </Button>
+        <Button>
+        le texte change alors que c'est le meme composant
+        </Button>
+    </div>
+    )
   }
 }
 
---
+++ src/App2.jsx
@@ -0,0 +1,20 @@
+const App2 = () => {  
+  const [count, setCount] = useState(0);
+  function updateMyCount (){
+    setCount(count + 1)
+  }
+    return (
+      <div className="App" style={{border: '2px solid green', padding: 20}}>
+        {count}
+        <Button onClick={() => updateMyCount()}>
+          mon texte dynamique
+          </Button>
+          <Button>
+          le texte change alors que c'est le meme composant
+          </Button>
+      </div>
+    );
+}
+
+export default App2;
+
